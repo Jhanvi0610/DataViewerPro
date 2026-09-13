d0a3d0e1237fa48d98d9be9216027f324dbd4f55
Jhanvi0610 <chandranjhanvi35@gmail.com>
2026-09-13 18:48:11 +0530

    Remove build artifacts, add gitignore

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DataLogin_FILE C:/Users/jhanv/OneDrive/Desktop/Projects/DataLogin/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/DataLogin.exe)
-set(__QT_DEPLOY_TARGET_DataLogin_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DataLogin_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Svg.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/DataLogin.dir/DataLogin_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/DataLogin.dir/DataLogin_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- C:\Users\jhanv\OneDrive\Desktop\Projects\DataLogin\build\Desktop_Qt_6_11_1_MinGW_64_bit_Debug\DataLogin_autogen\EWIEGA46WW\qrc_resources.cpp
